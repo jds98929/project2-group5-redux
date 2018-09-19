c3149d03f121a8f5ad8ab15e7422cad247a9045c
Tyler Couzens <ty.couzens@gmail.com>
2018-09-19 08:45:19 -0400

    added a border to the sign in component

--- redux-sass/src/components/sign-in/sign-in.component.tsx
+++ redux-sass/src/components/sign-in/sign-in.component.tsx
@@ -68,7 +68,8 @@ class SignInComponent extends React.Component<IProps, {}> {
     //   </div>
     // </div>
       <div>
-        <form className="form-signin" onSubmit={this.submit}>
+        <form className="form-signin border border-primary" onSubmit={this.submit}>
+        
           <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
 
           <label htmlFor="inputUsername" className="sr-only">Username</label>
@@ -98,7 +99,7 @@ class SignInComponent extends React.Component<IProps, {}> {
         <div id="sign-in-row"> 
           <div className="col-sm-4">
             <br />
-            <img src={logo2}className="rounded-circle" width="255" height="255"></img> 
+            <img src={logo2}className="rounded-circle" width="255" height="200"></img> 
           </div>
         </div>
         </div>
