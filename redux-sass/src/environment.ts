d4ddbc38c82d24ff8789f63ce292f50baa5b6682
Jacob Shaw <jds98929@uga.edu>
2018-09-22 20:47:20 -0400

    test

--- redux-sass/src/environment.ts
+++ redux-sass/src/environment.ts
@@ -3,7 +3,7 @@ const dev = {
 }
 
 const prod = {
-  context: 'http://ec2-18-217-243-52.us-east-2.compute.amazonaws.com:3001/'
+  context: 'http://ec2-18-217-243-52.us-east-2.compute.amazonaws.com:8080/'
 }
 
 export const environment = process.env.NODE_ENV === 'production'
